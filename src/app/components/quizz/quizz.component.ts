e2a6518ad69d017003253b8956b476613360a9ad
Davi <davisilvaqueiroz@outlook.com>
2023-04-25 10:32:57 -0300

    criando regras do jogo

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import quizz_questions from '../../../assets/data/quizz-questions.json';
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
   styleUrls: ['./quizz.component.css']
 })
-export class QuizzComponent {
+export class QuizzComponent implements OnInit {
   title: string ='';
   questions: any;
   questionSelected: any;
@@ -17,4 +19,30 @@ export class QuizzComponent {
   questionMaxIndex:number = 0;
 
   finished:boolean = false;
+
+  ngOnInit(): void {
+   if(quizz_questions){
+    this.finished = false;
+    this.title = quizz_questions.title;
+
+    this.questions = quizz_questions.questions;
+    this.questionSelected = quizz_questions.questions[this.questionIndex]
+
+    this.questionMaxIndex = quizz_questions.questions.length;
+   }
+  }
+  playerChoose(value: string): void {
+    this.answers.push(value)
+    console.log(this.answers)
+  }
+
+  async nextStep():Promise<any>{
+    this.questionIndex+=1;
+
+    if(this.questionMaxIndex > this.questionIndex){
+      this.questionSelected = this.questions[this.questionIndex];
+    }else{
+      this.finished = true;
+    }
+  }
 }
